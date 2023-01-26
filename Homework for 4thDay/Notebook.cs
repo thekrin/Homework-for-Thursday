82d2278619aef5b8924293a63d70352d51a95004
thekrin <elvineba@code.edu.az>
2023-01-26 14:18:45 +0400

    ders

---
+++ Homework for 4thDay/Notebook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Homework_for_4thDay
+{
+    internal class Notebook
+    {
+        public Notebook(string brand, string model, double price = 0)
+        {
+           this.Brand=brand;
+           this.Model=model;
+           this.Price=price;
+        }
+
+            public string Brand;
+            public string Model;
+            public double Price;
+
+        public string ShowInfo()
+        {
+            return $"Brand: {Brand}, Model: {Model}, Price: {Price}";
+        }
+    }
+    
+
+    
+}
---
+++ Homework for 4thDay/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Homework_for_4thDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook[] Laptop = new Notebook[3];
+
+            for (int i = 0; i < Laptop.Length; i++)
+            {
+                Console.WriteLine("Hansi Brand oldugunu daxil edin :");
+                string brand= Console.ReadLine();
+
+                Console.WriteLine("Hansi Model oldugunu daxil edin:");
+                string model= Console.ReadLine();
+
+                double price;
+                Console.WriteLine("Qiymetini daxil edin:");
+                string Price=Console.ReadLine();
+                price = Convert.ToDouble(Price);
+
+               
+                Laptop[i] =  new Notebook(brand, model, price);
+            }
+
+            double sum = 0;
+            for (int i = 0; i < Laptop.Length; i++)
+            {
+                sum += Laptop[i].Price;            
+            }
+
+            double average = sum / Laptop.Length;
+            Console.WriteLine(average);
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
